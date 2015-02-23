b74ec5d7d273e4598234de05a43a5e19308a3584
ctuimolo <ctuimolo@ucsc.edu>
2015-02-22 20:26:56 -0800

    Main source code 0.2
    Error in changing rooms, the mouseDown listener is still active from the
    previous room

--- main/main.js
+++ main/main.js
@@ -31,22 +31,43 @@ var room_manager = new Sprite();
  * be saved for that room's purpose, as a room sprite is
  * a collection of static methods and private variables. */
 var Room_00 = new Sprite();
+	// Create this room
+	/* A room's create() method is to be called in order
+	 * to initialize any of the room's needed objects. */
 	Room_00.create = function() {
+		// Create room sprite objects
+		/* Sprite objects can contain static methods that
+		 * invoke their specific action. In these cases
+		 * the sprite objects are buttons with an onDown() 
+		 * method that is to be called on action. */
 		var button_next = new Sprite();
 			button_next.width  = 150;
 			button_next.height = 90;
 			button_next.x = 930;
 			button_next.y = 630;
 			button_next.image  = Textures.load("./Common/Textures Demo/Next.png");
+			button_next.onDown = function() { changeRoom(room_01); }
 		world.addChild(button_next);
 		
 		// Create an input manager for this room.
 		/* The input manager is specific to each room
 		 * and created only at the room creation time.
 		 */
-		var input_manager = new Sprite();
-		gInput.addMouseDownListener(input_manager);
+		Room_00.input_manager = new Sprite();
+		gInput.addMouseDownListener(Room_00.input_manager);
+		Room_00.input_manager.onMouseDown = function(button){
+			alert("hey");
+		}
 	}
+	
+	// Clear this room
+	/* A room must have its unwanted sprites or other 
+	 * noisy bits cleared from the world as the game moves 
+	 * to different rooms. */
+	 Room_00.clear = function() {
+		 world.removeChild(button_next);
+	 }
+	
 
 var Room_01 = new Sprite();
 	Room_01.create = function() {
@@ -73,6 +94,16 @@ var Room_01 = new Sprite();
 world.addChild(room_manager);
 room_manager.curr_room = Room_00;
 room_manager.curr_room.create();
+
+changeRoom(Room_01);
+
+// Game functions
+/* Global game functions that mostly deal with changing rooms. */
+function changeRoom(room) {
+	//room_manager.curr_room.clear();
+	room_manager.curr_room = room;
+	room_manager.curr_room.create();
+}
 
 
 
