c1ef70d8025b870a98b2100cab584a91862cde7a
ctuimolo <ctuimolo@ucsc.edu>
2015-02-23 20:33:44 -0800

    Room demo code 0.5
    + 4 rooms are now correctly implemented. Insert some alerts at room
    creation time if you wish to check.
    + Global event listener for the mouse clicks is correctly implemented
    + Each active sprite (which should be pushed onto active_sprite array)
    should contain a click() function to do its action
    
    - Need to add several toggle buttons in each room to check variable
    saving across multiple room traversal.
    - Code might be a bit redundant?

--- main/main.js
+++ main/main.js
@@ -18,18 +18,18 @@ var active_sprites = new Array();
 /* The input manager calls any active sprite in the
  * active_sprites array's click(); function. */
 input_manager = new Sprite();
-	input_manager.onMouseDown = function() {
-		// For each active sprite in room...  
-		for(var sprite in active_sprites){
-			sprite = active_sprites[sprite];
-			// If we are clicking on that sprite...
-			if(checkSprite(sprite, gInput.mouse.x, gInput.mouse.y)){
-				// Do that sprites on click function...
-				sprite.click();
-				break;
-			}
+input_manager.onMouseDown = function() {
+	// For each active sprite in room...  
+	for(var sprite in active_sprites){
+		sprite = active_sprites[sprite];
+		// If we are clicking on that sprite...
+		if(checkSprite(sprite, gInput.mouse.x, gInput.mouse.y)) {
+			// Do that sprites on click function...
+			sprite.click();
+			break;
 		}
 	}
+}
 
 // Create room manager
 /* The Room manager is a sprite handler that 
@@ -41,39 +41,43 @@ var room_manager = new Sprite();
 // Create room object
 /* A room sprite should contain the elements necessary to
  * be saved for that room's purpose, as a room sprite is
- * a collection of static methods and private variables. */
+ * a collection of static methods and variables. */
 var Room_00 = new Sprite();
-	// Initialize room sprites
-	/* Sprite objects can contain static methods that
-	 * invoke their specific action. In these cases
-	 * the sprite objects are buttons with an onDown() 
-	 * method that is to be called on action. */
-	var button_next = new Sprite();
-		button_next.width  = 150;
-		button_next.height = 90;
-		button_next.x = 930;
-		button_next.y = 630;
-		button_next.image  = Textures.load("./Common/Textures Demo/Next.png");
-		button_next.click = function() {
-			alert("You clicked Room 00's -Next- button");
-		}
 
-	var button_on = new Sprite();
-		button_on.width  = 150;
-		button_on.height = 90;
-		button_on.image  = Textures.load("./Common/Textures Demo/On.png");
-
-	var button_off = new Sprite();
-		button_off.width  = 150;
-		button_off.height = 90;
-		button_off.image  = Textures.load("./Common/Textures Demo/Off.png");
-	
 	// Create this room
 	/* A room's create() method is to be called in order
 	 * to initialize any of the room's needed objects. */
 	Room_00.create = function() {
-		world.addChild(button_next);
-		active_sprites.push(button_next);
+		
+		// Initialize room sprites
+		/* Sprite objects can contain static methods that
+		* invoke their specific action. In these cases
+		* the sprite objects are buttons with an onDown() 
+		* method that is to be called on action. */
+		Room_00.button_next = new Sprite();
+		Room_00.button_next.width  = 150;
+		Room_00.button_next.height = 90;
+		Room_00.button_next.x = 930;
+		Room_00.button_next.y = 630;
+		Room_00.button_next.image  = Textures.load("./Common/Textures Demo/Next.png");
+		Room_00.button_next.click = function() { changeRoom(Room_01); }
+			
+		Room_00.button_on = new Sprite();
+		Room_00.button_on.width  = 150;
+		Room_00.button_on.height = 90;
+		Room_00.button_on.image  = Textures.load("./Common/Textures Demo/On.png");
+
+		Room_00.button_off = new Sprite();
+		Room_00.button_off.width  = 150;
+		Room_00.button_off.height = 90;
+		Room_00.button_off.image  = Textures.load("./Common/Textures Demo/Off.png");
+
+		
+		// Add to the world the sprites necessary
+		world.addChild(Room_00.button_next);
+		
+		// Push active sprites onto array
+		active_sprites.push(Room_00.button_next);
 	}
 	
 	// Clear this room
@@ -81,77 +85,149 @@ var Room_00 = new Sprite();
 	/* the game switches rooms. Listeners and sprites to be
 	/* removed from the world are removed here. */
 	Room_00.clear = function() {
-		gInput.removeMouseDownListener(Room_00.input_manager);
-		world.remove(button_next);
+		world.removeChild(Room_00.button_next);
+		while(active_sprites.length > 0)
+			active_sprites.pop();
 	}
 	
 var Room_01 = new Sprite();
-	// Initialize room sprites
-	var button_next = new Sprite();
-		button_next.width  = 150;
-		button_next.height = 90;
-		button_next.x = 930;
-		button_next.y = 630;
-		button_next.image  = Textures.load("./Common/Textures Demo/Next.png");
-
-	var button_prev = new Sprite();
-		button_prev.width  = 150;
-		button_prev.height = 90;
-		button_prev.x = 0;
-		button_prev.y = 630;
-		button_prev.image  = Textures.load("./Common/Textures Demo/Previous.png");
 
 	// Create this room
 	Room_01.create = function() {
-		world.addChild(button_next);
-		world.addChild(button_prev);
-		active_sprites.push(button_next);
-		active_sprites.push(button_prev);
+		
+		// Initialize room sprites
+		Room_01.button_next = new Sprite();
+		Room_01.button_next.width  = 150;
+		Room_01.button_next.height = 90;
+		Room_01.button_next.x = 930;
+		Room_01.button_next.y = 630;
+		Room_01.button_next.image  = Textures.load("./Common/Textures Demo/Next.png");
+		Room_01.button_next.click = function() { changeRoom(Room_02); }
+
+		Room_01.button_prev = new Sprite();
+		Room_01.button_prev.width  = 150;
+		Room_01.button_prev.height = 90;
+		Room_01.button_prev.x = 0;
+		Room_01.button_prev.y = 630;
+		Room_01.button_prev.image  = Textures.load("./Common/Textures Demo/Previous.png");
+		Room_01.button_prev.click = function() { changeRoom(Room_00); }
+
+		// Add to the world the sprites necessary
+		world.addChild(Room_01.button_next);
+		world.addChild(Room_01.button_prev);
+		
+		// Push active sprites onto the array
+		active_sprites.push(Room_01.button_next);
+		active_sprites.push(Room_01.button_prev);
 	}
 	
 	// Clear this room
 	Room_01.clear = function() {
-		world.remove(button_next);
-		world.remove(button_prev);
+		world.remove(Room_01.button_next);
+		world.remove(Room_01.button_prev);
+		while(active_sprites.length > 0)
+			active_sprites.pop();
+	}
+	
+var Room_02 = new Sprite();
+
+	// Create this room
+	Room_02.create = function() {
+		
+		// Initialize room sprites
+		Room_02.button_next = new Sprite();
+		Room_02.button_next.width  = 150;
+		Room_02.button_next.height = 90;
+		Room_02.button_next.x = 930;
+		Room_02.button_next.y = 630;
+		Room_02.button_next.image  = Textures.load("./Common/Textures Demo/Next.png");
+		Room_02.button_next.click = function() { changeRoom(Room_03); }
+
+		Room_02.button_prev = new Sprite();
+		Room_02.button_prev.width  = 150;
+		Room_02.button_prev.height = 90;
+		Room_02.button_prev.x = 0;
+		Room_02.button_prev.y = 630;
+		Room_02.button_prev.image  = Textures.load("./Common/Textures Demo/Previous.png");
+		Room_02.button_prev.click = function() { changeRoom(Room_01); }
+
+		// Add to the world the sprites necessary
+		world.addChild(Room_02.button_next);
+		world.addChild(Room_02.button_prev);
+		
+		// Push active sprites onto the array
+		active_sprites.push(Room_02.button_next);
+		active_sprites.push(Room_02.button_prev);
+	}
+	
+	// Clear this room
+	Room_02.clear = function() {
+		world.remove(Room_02.button_next);
+		world.remove(Room_02.button_prev);
+		while(active_sprites.length > 0)
+			active_sprites.pop();
+	}
+
+var Room_03 = new Sprite();
+
+	// Create this room
+	Room_03.create = function() {
+		
+		// Initialize room sprites
+		Room_03.button_prev = new Sprite();
+		Room_03.button_prev.width  = 150;
+		Room_03.button_prev.height = 90;
+		Room_03.button_prev.x = 0;
+		Room_03.button_prev.y = 630;
+		Room_03.button_prev.image  = Textures.load("./Common/Textures Demo/Previous.png");
+		Room_03.button_prev.click = function() { changeRoom(Room_02); }
+
+		// Add to the world the sprites necessary
+		world.addChild(Room_03.button_prev);
+		
+		// Push active sprites onto the array
+		active_sprites.push(Room_03.button_prev);
+	}
+	
+	// Clear this room
+	Room_03.clear = function() {
+		world.remove(Room_03.button_prev);
+		while(active_sprites.length > 0)
+			active_sprites.pop();
 	}
 
 // Initialize Brine's world
 /* These are the only necessary objects needed
    to begin the game. */
 world.addChild(room_manager);
-room_manager.curr_room = Room_00;
-room_manager.curr_room.create();
-
 world.addChild(input_manager);
 
-changeRoom(Room_01);
-changeRoom(Room_00);
+room_manager.curr_room = Room_00;
+room_manager.curr_room.create();
 
 // Event listeners to be used in the game always
 gInput.addMouseDownListener(input_manager);
 
 // Game functions
-/* Global game functions that mostly deal with changing rooms. */
+/* Global game functions that mostly deal with changing rooms,
+ * and whatever listeners are necessary. */
 // Changes rooms
 function changeRoom(room) {
 	room_manager.curr_room.clear();
-	while(active_sprites.length > 0)
-		active_sprites.pop();
-	
 	room_manager.curr_room = room;
 	room_manager.curr_room.create();
 }
 
-// Returns the sprite the mouse is over
+// Returns true if the mouse is over a sprite.
 function checkSprite(sprite, x, y){
-  var minX = sprite.x;
-  var maxX = sprite.x+sprite.width;
-  var minY = sprite.y;
-  var maxY = sprite.y+sprite.height;
+  var min_x = sprite.x;
+  var max_x = sprite.x + sprite.width;
+  var min_y = sprite.y;
+  var max_y = sprite.y + sprite.height;
   var mx = x;
   var my = y;
   
-  if(mx >= minX && mx <= maxX && my >= minY && my <= maxY){
+  if(mx >= min_x && mx <= max_x && my >= min_y && my <= max_y){
     return true;
   }
   return false;
