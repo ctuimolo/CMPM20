90a3f68c4abe5ce38fff8dd14a9053f09bd94d8b
ctuimolo <ctuimolo@ucsc.edu>
2015-02-23 22:07:36 -0800

    Room demo code 1.0
    MISSION COMPLETED ( v1.0 )
    + Room integration works off of different sprite objects
    + World persistence demonstrated through each object containing its own
    private booleans
    + Click on the switches in each room to switch the booleans and observe
    how the values are saved even as you traverse the rooms
    
    - Code might be somewhat redundant?
    - Improvements could involve streamlining this code as much as possible
    - More efficient means of allocating rooms or room objects?

--- main/main.js
+++ main/main.js
@@ -43,6 +43,8 @@ var room_manager = new Sprite();
  * be saved for that room's purpose, as a room sprite is
  * a collection of static methods and variables. */
 var Room_00 = new Sprite();
+	Room_00.switch01 = false;
+	Room_00.switch02 = false;
 
 	// Create this room
 	/* A room's create() method is to be called in order
@@ -61,23 +63,66 @@ var Room_00 = new Sprite();
 		Room_00.button_next.y = 630;
 		Room_00.button_next.image  = Textures.load("./Common/Textures Demo/Next.png");
 		Room_00.button_next.click = function() { changeRoom(Room_01); }
-			
-		Room_00.button_on = new Sprite();
-		Room_00.button_on.width  = 150;
-		Room_00.button_on.height = 90;
-		Room_00.button_on.image  = Textures.load("./Common/Textures Demo/On.png");
 
-		Room_00.button_off = new Sprite();
-		Room_00.button_off.width  = 150;
-		Room_00.button_off.height = 90;
-		Room_00.button_off.image  = Textures.load("./Common/Textures Demo/Off.png");
-
+		Room_00.button_switch01 = new Sprite();
+		Room_00.button_switch01.width  = 150;
+		Room_00.button_switch01.height = 90;
+		Room_00.button_switch01.x = 96
+		Room_00.button_switch01.y = 232
+		Room_00.button_switch01.click = function() {
+			if(Room_00.switch01) {
+				Room_00.button_switch01.image =
+					Textures.load("./Common/Textures Demo/Off.png");
+				Room_00.switch01 = false;
+			}else {
+				Room_00.button_switch01.image = 
+					Textures.load("./Common/Textures Demo/On.png");
+				Room_00.switch01 = true;
+			}
+		}
 		
+		Room_00.button_switch02 = new Sprite();
+		Room_00.button_switch02.width  = 150;
+		Room_00.button_switch02.height = 90;
+		Room_00.button_switch02.x = 96
+		Room_00.button_switch02.y = 398
+		Room_00.button_switch02.click = function() {
+			if(Room_00.switch02) {
+				Room_00.button_switch02.image =
+					Textures.load("./Common/Textures Demo/Off.png");
+				Room_00.switch02 = false;
+			}else {
+				Room_00.button_switch02.image = 
+					Textures.load("./Common/Textures Demo/On.png");
+				Room_00.switch02 = true;
+			}
+		}
+		
+		if(Room_00.switch01) {
+				Room_00.button_switch01.image =
+					Textures.load("./Common/Textures Demo/On.png");
+			}else {
+				Room_00.button_switch01.image = 
+					Textures.load("./Common/Textures Demo/Off.png");
+			}
+			
+		if(Room_00.switch02) {
+				Room_00.button_switch02.image =
+					Textures.load("./Common/Textures Demo/On.png");
+			}else {
+				Room_00.button_switch02.image = 
+					Textures.load("./Common/Textures Demo/Off.png");
+			}
+	
 		// Add to the world the sprites necessary
 		world.addChild(Room_00.button_next);
+		world.addChild(Room_00.button_switch01);
+		world.addChild(Room_00.button_switch02);
 		
 		// Push active sprites onto array
 		active_sprites.push(Room_00.button_next);
+		active_sprites.push(Room_00.button_switch01);
+		active_sprites.push(Room_00.button_switch02);
 	}
 	
 	// Clear this room
@@ -86,11 +131,16 @@ var Room_00 = new Sprite();
 	/* removed from the world are removed here. */
 	Room_00.clear = function() {
 		world.removeChild(Room_00.button_next);
+		world.removeChild(Room_00.button_switch01);
+		world.removeChild(Room_00.button_switch02);
+		
 		while(active_sprites.length > 0)
 			active_sprites.pop();
 	}
 	
 var Room_01 = new Sprite();
+	Room_01.switch01 = false;
+	Room_01.switch01 = false;
 
 	// Create this room
 	Room_01.create = function() {
@@ -111,25 +161,83 @@ var Room_01 = new Sprite();
 		Room_01.button_prev.y = 630;
 		Room_01.button_prev.image  = Textures.load("./Common/Textures Demo/Previous.png");
 		Room_01.button_prev.click = function() { changeRoom(Room_00); }
+		
+		Room_01.button_switch01 = new Sprite();
+		Room_01.button_switch01.width  = 150;
+		Room_01.button_switch01.height = 90;
+		Room_01.button_switch01.x = 342
+		Room_01.button_switch01.y = 232
+		Room_01.button_switch01.click = function() {
+			if(Room_01.switch01) {
+				Room_01.button_switch01.image =
+					Textures.load("./Common/Textures Demo/Off.png");
+				Room_01.switch01 = false;
+			}else {
+				Room_01.button_switch01.image = 
+					Textures.load("./Common/Textures Demo/On.png");
+				Room_01.switch01 = true;
+			}
+		}
+		
+		Room_01.button_switch02 = new Sprite();
+		Room_01.button_switch02.width  = 150;
+		Room_01.button_switch02.height = 90;
+		Room_01.button_switch02.x = 342
+		Room_01.button_switch02.y = 398
+		Room_01.button_switch02.click = function() {
+			if(Room_01.switch02) {
+				Room_01.button_switch02.image =
+					Textures.load("./Common/Textures Demo/Off.png");
+				Room_01.switch02 = false;
+			}else {
+				Room_01.button_switch02.image = 
+					Textures.load("./Common/Textures Demo/On.png");
+				Room_01.switch02 = true;
+			}
+		}
 
+		if(Room_01.switch01) {
+				Room_01.button_switch01.image =
+					Textures.load("./Common/Textures Demo/On.png");
+			}else {
+				Room_01.button_switch01.image = 
+					Textures.load("./Common/Textures Demo/Off.png");
+			}
+			
+		if(Room_01.switch02) {
+				Room_01.button_switch02.image =
+					Textures.load("./Common/Textures Demo/On.png");
+			}else {
+				Room_01.button_switch02.image = 
+					Textures.load("./Common/Textures Demo/Off.png");
+			}
+		
 		// Add to the world the sprites necessary
 		world.addChild(Room_01.button_next);
 		world.addChild(Room_01.button_prev);
+		world.addChild(Room_01.button_switch01);
+		world.addChild(Room_01.button_switch02);
 		
 		// Push active sprites onto the array
 		active_sprites.push(Room_01.button_next);
 		active_sprites.push(Room_01.button_prev);
+		active_sprites.push(Room_01.button_switch01);
+		active_sprites.push(Room_01.button_switch02);
 	}
 	
 	// Clear this room
 	Room_01.clear = function() {
 		world.remove(Room_01.button_next);
 		world.remove(Room_01.button_prev);
+		world.remove(Room_01.button_switch01);
+		world.remove(Room_01.button_switch02);
 		while(active_sprites.length > 0)
 			active_sprites.pop();
 	}
 	
 var Room_02 = new Sprite();
+	Room_02.switch01 = false;
+	Room_02.switch02 = false;
 
 	// Create this room
 	Room_02.create = function() {
@@ -150,20 +258,76 @@ var Room_02 = new Sprite();
 		Room_02.button_prev.y = 630;
 		Room_02.button_prev.image  = Textures.load("./Common/Textures Demo/Previous.png");
 		Room_02.button_prev.click = function() { changeRoom(Room_01); }
+		
+		Room_02.button_switch01 = new Sprite();
+		Room_02.button_switch01.width  = 150;
+		Room_02.button_switch01.height = 90;
+		Room_02.button_switch01.x = 588
+		Room_02.button_switch01.y = 232
+		Room_02.button_switch01.click = function() {
+			if(Room_02.switch01) {
+				Room_02.button_switch01.image =
+					Textures.load("./Common/Textures Demo/Off.png");
+				Room_02.switch01 = false;
+			}else {
+				Room_02.button_switch01.image = 
+					Textures.load("./Common/Textures Demo/On.png");
+				Room_02.switch01 = true;
+			}
+		}
+		
+		Room_02.button_switch02 = new Sprite();
+		Room_02.button_switch02.width  = 150;
+		Room_02.button_switch02.height = 90;
+		Room_02.button_switch02.x = 588
+		Room_02.button_switch02.y = 398
+		Room_02.button_switch02.click = function() {
+			if(Room_02.switch02) {
+				Room_02.button_switch02.image =
+					Textures.load("./Common/Textures Demo/Off.png");
+				Room_02.switch02 = false;
+			}else {
+				Room_02.button_switch02.image = 
+					Textures.load("./Common/Textures Demo/On.png");
+				Room_02.switch02 = true;
+			}
+		}
+
+		if(Room_02.switch01) {
+				Room_02.button_switch01.image =
+					Textures.load("./Common/Textures Demo/On.png");
+			}else {
+				Room_02.button_switch01.image = 
+					Textures.load("./Common/Textures Demo/Off.png");
+			}
+			
+		if(Room_02.switch02) {
+				Room_02.button_switch02.image =
+					Textures.load("./Common/Textures Demo/On.png");
+			}else {
+				Room_02.button_switch02.image = 
+					Textures.load("./Common/Textures Demo/Off.png");
+			}
 
 		// Add to the world the sprites necessary
 		world.addChild(Room_02.button_next);
 		world.addChild(Room_02.button_prev);
+		world.addChild(Room_02.button_switch01);
+		world.addChild(Room_02.button_switch02);
 		
 		// Push active sprites onto the array
 		active_sprites.push(Room_02.button_next);
 		active_sprites.push(Room_02.button_prev);
+		active_sprites.push(Room_02.button_switch01);
+		active_sprites.push(Room_02.button_switch02);
 	}
 	
 	// Clear this room
 	Room_02.clear = function() {
 		world.remove(Room_02.button_next);
 		world.remove(Room_02.button_prev);
+		world.remove(Room_02.button_switch01);
+		world.remove(Room_02.button_switch02);
 		while(active_sprites.length > 0)
 			active_sprites.pop();
 	}
@@ -181,17 +345,73 @@ var Room_03 = new Sprite();
 		Room_03.button_prev.y = 630;
 		Room_03.button_prev.image  = Textures.load("./Common/Textures Demo/Previous.png");
 		Room_03.button_prev.click = function() { changeRoom(Room_02); }
+		
+		Room_03.button_switch01 = new Sprite();
+		Room_03.button_switch01.width  = 150;
+		Room_03.button_switch01.height = 90;
+		Room_03.button_switch01.x = 834
+		Room_03.button_switch01.y = 232
+		Room_03.button_switch01.click = function() {
+			if(Room_03.switch01) {
+				Room_03.button_switch01.image =
+					Textures.load("./Common/Textures Demo/Off.png");
+				Room_03.switch01 = false;
+			}else {
+				Room_03.button_switch01.image = 
+					Textures.load("./Common/Textures Demo/On.png");
+				Room_03.switch01 = true;
+			}
+		}
+		
+		Room_03.button_switch02 = new Sprite();
+		Room_03.button_switch02.width  = 150;
+		Room_03.button_switch02.height = 90;
+		Room_03.button_switch02.x = 834
+		Room_03.button_switch02.y = 398
+		Room_03.button_switch02.click = function() {
+			if(Room_03.switch02) {
+				Room_03.button_switch02.image =
+					Textures.load("./Common/Textures Demo/Off.png");
+				Room_03.switch02 = false;
+			}else {
+				Room_03.button_switch02.image = 
+					Textures.load("./Common/Textures Demo/On.png");
+				Room_03.switch02 = true;
+			}
+		}
+
+		if(Room_03.switch01) {
+				Room_03.button_switch01.image =
+					Textures.load("./Common/Textures Demo/On.png");
+			}else {
+				Room_03.button_switch01.image = 
+					Textures.load("./Common/Textures Demo/Off.png");
+			}
+			
+		if(Room_03.switch02) {
+				Room_03.button_switch02.image =
+					Textures.load("./Common/Textures Demo/On.png");
+			}else {
+				Room_03.button_switch02.image = 
+					Textures.load("./Common/Textures Demo/Off.png");
+			}
 
 		// Add to the world the sprites necessary
 		world.addChild(Room_03.button_prev);
+		world.addChild(Room_03.button_switch01);
+		world.addChild(Room_03.button_switch02);
 		
 		// Push active sprites onto the array
 		active_sprites.push(Room_03.button_prev);
+		active_sprites.push(Room_03.button_switch01);
+		active_sprites.push(Room_03.button_switch02);
 	}
 	
 	// Clear this room
 	Room_03.clear = function() {
 		world.remove(Room_03.button_prev);
+		world.remove(Room_03.button_switch01);
+		world.remove(Room_03.button_switch02);
 		while(active_sprites.length > 0)
 			active_sprites.pop();
 	}
